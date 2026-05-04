a109ef54e539f0cfc88d1b8d1afa6f6d2b844c68
susiy <susiy@DAMIAN>
2026-05-04 12:44:14 -0600

    commit inicial

--- src/supermercado/Verduras.java
+++ src/supermercado/Verduras.java
@@ -3,27 +3,30 @@ package supermercado;
 public class Verduras extends Producto{
     //Atributos
     private double pesoKg;
-    private String tipoUnidad;
     
     //constructor
 
     public Verduras() {
     }
     
-    public Verduras(double pesoKg, String tipoUnidad, double precioBase, String nombre, String marca, String categoria, int id, int stock, int cantidad) {
+    public Verduras(double pesoKg, double precioBase, String nombre, String marca, String categoria, int id, int stock, int cantidad) {
         super(precioBase, nombre, marca, categoria, id, stock, cantidad);
         this.pesoKg = pesoKg;
-        this.tipoUnidad = tipoUnidad;
     }
 
     public Verduras(double pesoKg, String tipoUnidad) {
         this.pesoKg = pesoKg;
-        this.tipoUnidad = tipoUnidad;
     }
 
     @Override
     public double calcularPrecioFinal() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //incremento por desperdicio
+        double precioVerdura = this.precioBase*.4;
+        return precioVerdura;
     }
-    
+
+    @Override
+    public String toString() {
+        return String.format("Verduras {peso: %f kg, precio: %.2f}", pesoKg,calcularPrecioFinal());
+    }
 }
