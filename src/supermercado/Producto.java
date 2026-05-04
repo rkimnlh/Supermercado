f8a0484f21f8a890663c64ea718447b33a05a861
juan6 <juan6@172.17.186.95>
2026-05-04 10:53:52 -0600

    commit inicial

--- src/supermercado/Producto.java
+++ src/supermercado/Producto.java
@@ -1,7 +1,6 @@
 
 package supermercado;
 
-
 public abstract class Producto {
     protected double precioBase;
     protected String nombre;
@@ -9,4 +8,31 @@ public abstract class Producto {
     protected String categoria;
     protected int id;
     protected int stock;
+
+    // Constructor
+    public Producto (double precioBase, String nombre, String marca, String categoria, int id, int stock) {
+        this.precioBase = precioBase;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.categoria = categoria;
+        this.id = id;
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "precioBase=" + precioBase +
+                ", nombre='" + nombre + '\'' +
+                ", marca='" + marca + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", id=" + id +
+                ", stock=" + stock +
+                '}';
+    }
+
+    // Método abstracto
+    public abstract double calcularPrecioFinal();
+
 }
+
