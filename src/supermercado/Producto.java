f33590e7b02f0306eeb7e150944ba590a9615ef3
juan6 <juan6@172.17.186.95>
2026-05-04 10:43:52 -0600

    commit inicial

---
+++ src/supermercado/Producto.java
@@ -0,0 +1,12 @@
+
+package supermercado;
+
+
+public abstract class Producto {
+    protected double precioBase;
+    protected String nombre;
+    protected String marca;
+    protected String categoria;
+    protected int id;
+    protected int stock;
+}
