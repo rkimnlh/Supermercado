f57db080bdd02bdf16b99f5ae476ff015ea4c032
juan6 <juan6@172.17.186.95>
2026-05-25 12:27:29 -0600

    commit Venta

---
+++ src/supermercado/Venta.java
@@ -0,0 +1,19 @@
+package supermercado;
+import java.io.Serializable;
+
+public class Venta implements Serializable {
+    private String nombreProducto;
+    private int cantidadVendida;
+    private double importeTotal;
+
+    public Venta(String nombreProducto, int cantidadVendida, double importeTotal) {
+        this.nombreProducto = nombreProducto;
+        this.cantidadVendida = cantidadVendida;
+        this.importeTotal = importeTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto " + nombreProducto + " | Cantidad: " + cantidadVendida + " | Total: $" + importeTotal;
+    }
+}
