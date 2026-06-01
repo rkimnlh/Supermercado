beb2f4c9ed23adbf960da56520f14217eaa26cc9
juan6 <juan6@172.17.186.95>
2026-05-31 21:21:14 -0600

    commit Mensaje

--- src/supermercado/Mensaje.java
+++ src/supermercado/Mensaje.java
@@ -1,5 +1,6 @@
 package supermercado;
 public interface Mensaje {
+   // metodo abstracto que cada clase tendra que implementar, esto muestra un "detalle" o recomendacion sobre el producto
     public String mostrarDetalle(); 
 }
 
